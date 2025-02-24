7e5ec2000949c39823e0ad13c5969956d2672ab1
stromek <petr.biza@gmail.com>
2025-02-24 16:21:39 +0100

    fix php-dev public URL s queryStringem

--- htdocs/cli-router.php
+++ htdocs/cli-router.php
@@ -12,24 +12,28 @@ if(php_sapi_name() !== 'cli-server') {
     "/public" => realpath(__DIR__ . "/../public")
   ];
 
-  foreach($directoryMap as $prefix => $directory) {
-    if(str_starts_with($_SERVER['REQUEST_URI'] ?? "", $prefix)) {
-      $file = $directory . DIRECTORY_SEPARATOR . substr($_SERVER['REQUEST_URI'], strlen($prefix) + 1);
+  $requestFile = $_SERVER['PHP_SELF'] ?? "";
+  if(strlen($requestFile)) {
+    foreach($directoryMap as $prefix => $directory) {
+      if(str_starts_with($requestFile, $prefix)) {
+        $file = $directory . DIRECTORY_SEPARATOR . substr($requestFile, strlen($prefix) + 1);
 
-      if(!file_exists($file) or !is_file($file)) {
-        http_response_code(404);
-        echo "<h1>Error 404</h1><p>File <em>{$_SERVER['REQUEST_URI']}</em> not found.</p>";
+
+        if(!file_exists($file) or !is_file($file)) {
+          http_response_code(404);
+          echo "<h1>Error 404</h1><p>File <em>{$requestFile}</em> not found.</p>";
+          exit(0);
+        }
+
+        $type = mime_content_type($file);
+        if($type) {
+          header("Content-Type: {$type}");
+          readfile($file, false);
+        }
         exit(0);
       }
-
-      $type = mime_content_type($file);
-      if($type) {
-        header("Content-Type: {$type}");
-        readfile($file, false);
-      }
-      exit(0);
     }
   }
 
-  return require __DIR__ . '/index.php';
+  return require(__DIR__ . '/index.php');
 })();
