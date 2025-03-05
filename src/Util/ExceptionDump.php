6a8c2be4a5ac5be7588ff91b271fb78ade8d8f58
stromek <petr.biza@gmail.com>
2025-03-05 08:50:00 +0100

    dump exception - odstraněny argumenty dumpu
    - fix recursive loop

--- src/Util/ExceptionDump.php
+++ src/Util/ExceptionDump.php
@@ -11,13 +11,17 @@ abstract class ExceptionDump {
    * @return array{message: string, code: int, file: string, line: int, trace: array<int, mixed>, class: class-string}
    */
   public static function toArray(\Throwable $Exception): array {
+    $trace = Arr::create($Exception->getTrace())
+      ->map(fn($Trace) => Arr::create($Trace)->cherryPickKey(["file", "line", "function", "class", "type"])->toArray())
+      ->toArray();
+
     return [
       "class" => $Exception::class,
       "message" => $Exception->getMessage(),
       "code" => $Exception->getCode(),
       "file" => $Exception->getFile(),
       "line" => $Exception->getLine(),
-      "trace" => $Exception->getTrace(),
+      "trace" => $trace,
       "previous" => $Exception->getPrevious() ? self::toArray($Exception->getPrevious()) : null,
     ];
   }
