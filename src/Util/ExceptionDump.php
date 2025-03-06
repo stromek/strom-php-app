cf51f09359d80721346830220da64c598bd90115
stromek <petr.biza@gmail.com>
2025-03-06 14:36:39 +0100

    Exception dump zobrazí SQL dotaz

--- src/Util/ExceptionDump.php
+++ src/Util/ExceptionDump.php
@@ -15,12 +15,18 @@ abstract class ExceptionDump {
       ->map(fn($Trace) => Arr::create($Trace)->cherryPickKey(["file", "line", "function", "class", "type"])->toArray())
       ->toArray();
 
+    $details = match(true) {
+      $Exception instanceof \Dibi\DriverException => $Exception->getSql(),
+      default => null
+    };
+
     return [
       "class" => $Exception::class,
       "message" => $Exception->getMessage(),
       "code" => $Exception->getCode(),
       "file" => $Exception->getFile(),
       "line" => $Exception->getLine(),
+      "details" => $details,
       "trace" => $trace,
       "previous" => $Exception->getPrevious() ? self::toArray($Exception->getPrevious()) : null,
     ];
